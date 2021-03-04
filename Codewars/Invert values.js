d19df6f3fbee40b2feea6a4cf1d411f206fc457b
Humberto455 <hrasgado3892@ppsc.edu.mx>
2021-03-04 16:38:52 -0600

    Invert Values

---
+++ Codewars/Invert values.js
@@ -0,0 +1,17 @@
+// Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+// invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+// invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+// invert([]) == []
+// You can assume that all values are integers. Do not mutate the input array/list.
+// // Code -->
+function invert(array) {
+  
+    let result = [];
+    
+    for(let i = 0; i < array.length;i++){
+      let resta = array[i] - (array[i] * 2);
+      const count = result.push(resta);
+  //       console.log(result);
+    }
+      return result;
+  }
